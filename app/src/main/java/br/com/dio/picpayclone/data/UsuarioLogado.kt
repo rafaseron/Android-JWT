abee23671f021cc961dcb62daddbb9da4dd8659b
rafaseron <rafaelseron3011@gmail.com>
2024-10-07 16:22:09 -0400

    feat: Variavel para salvar Token e funcoes para verificar isUserAuthenticated

--- app/src/main/java/br/com/dio/picpayclone/data/UsuarioLogado.kt
+++ app/src/main/java/br/com/dio/picpayclone/data/UsuarioLogado.kt
@@ -1,20 +1,25 @@
 package br.com.dio.picpayclone.data
 
 object UsuarioLogado {
+    lateinit var token: String
 
-    val usuario = Usuario(
-        "joaovf",
-        "123456",
-        "joaofreitas@gmail.com",
-        "João Vitor Freitas",
-        "62992920466",
-        "1980-06-20",
-        "62992920466",
-        0.0
-    )
+    val usuario =
+        Usuario(
+            "joaovf",
+            "123456",
+            "joaofreitas@gmail.com",
+            "João Vitor Freitas",
+            "62992920466",
+            "1980-06-20",
+            "62992920466",
+            0.0,
+        )
 
     fun setSaldo(saldo: Double) {
         usuario.saldo = saldo
     }
 
+    fun isAuthenticated(): Boolean = this::token.isInitialized
+
+    fun isNotAuthenticated(): Boolean = !isAuthenticated()
 }
